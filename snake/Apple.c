d0bffe8fdebf722c5c952ca50ea148066ffe365a
Steven Brooks <srbrooks@miners.utep.edu>
2017-11-30 16:14:08 -0700

    Added more apple code.

--- snake/Apple.c
+++ snake/Apple.c
@@ -1,7 +1,17 @@
 #include "Apple.h"
 
 Apple* appleInit() {
+	(Apple *)apple = malloc(sizeof(Apple)); // create apple
 	
+	(Layer *)appleLayer = malloc(sizeof(Layer)); // create and configure apple layer
+	appleLayer->abShape = &circle2;
+	appleLayer->posLast = {0,0};
+	appleLayer->pos = {screenWidth/2 + 10, screenHeight/2 + 10};
+	appleLayer->posNext = {0,0};
+	appleLayer->color = COLOR_RED;
+	appleLayer->next = 0;
+	
+	return apple;
 }
 
 void appleRespawn(Apple* apple);
