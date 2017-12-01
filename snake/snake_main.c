863689a3e2b3dd601d55e53993d801cd8a8951a0
Steven Brooks <srbrooks@miners.utep.edu>
2017-11-30 18:56:35 -0700

    More main code

--- snake/snake_main.c
+++ snake/snake_main.c
@@ -6,10 +6,47 @@
 #include "Snake.h"
 #include "Apple.h"
 
+Snake* snake = snakeInit();
+Apple* apple = appleInit();
+AbRectOutline field = {
+	abRectOutlineGetBounds, abRectOutlineCheck,
+	{screenWidth/2 - 10, screenHeight/2 - 10}
+}
+
+Layer fieldLayer = {
+	(AbShape *)&field,
+	{screenWidth/2, screenHeight/2},
+	{0,0},{0,0},
+	COLOR_BLACK,
+	snake->headLayer // next layer is the snake
+}
 
 void main() {
     // setup
     configureClocks();
     lcd_init();
     clearScreen(COLOR_BLUE);
+	
+	layerInit(&fieldLayer);
+	layerDraw(&fieldLayer);
+	
+	enableWDTInterrupts();
+	or_sr(0x8);
+	
+	for(;;) {
+		while(!redrawScreen) {
+			or_sr(0x10);
+		}
+		redrawScreen = 0;
+		snakeDraw(snake);
+	}
+}
+
+void wdt_c_handler() {
+	static short count = 0;
+	if (count == 15) {
+		snakeUpdate(snake);
+		count = 0;
+	}
+	count++;
 }
