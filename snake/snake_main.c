3c9b9d27265de38c1b6357ca0429e65797b5c5ec
Steven Brooks <srbrooks@miners.utep.edu>
2017-12-03 13:22:57 -0700

    Maybe fixed boundary issues?

--- snake/snake_main.c
+++ snake/snake_main.c
@@ -43,7 +43,7 @@ void main() {
   snakeInit();
   
   //appleInit();
-  layerGetBounds(&fieldLayer, &fieldFence);
+  abShapeGetBounds(&field, &fieldFence);
   
   layerInit(&fieldLayer);
   layerDraw(&fieldLayer);
